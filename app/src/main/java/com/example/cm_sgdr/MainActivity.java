ea24e8cc9483be7493ccfa150ee6b06bb0cab733
Henri48 <adaohenrique48@gmail.com>
2019-08-13 22:28:32 -0400

    update all

--- app/src/main/java/com/example/cm_sgdr/MainActivity.java
+++ app/src/main/java/com/example/cm_sgdr/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.cm_sgdr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.cm_sgdr.modelo.Pessoa;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
+    private List<Pessoa> listPessoa = new ArrayList<Pessoa>();
+    private ArrayAdapter<Pessoa> arrayAdapterPessoa;
+
+    Pessoa pessoaSelecionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +45,43 @@ public class MainActivity extends AppCompatActivity {
         listV_dados = (ListView)findViewById(R.id.listV_dados);
 
         inicializarFirebase();
+        eventoDatabase();
+
+        listV_dados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                pessoaSelecionada = (Pessoa)adapterView.getItemAtPosition(i);
+                edtNome.setText(pessoaSelecionada.getNome());
+                edtEmail.setText(pessoaSelecionada.getEmail());
+            }
+        });
+    }
+
+    private void eventoDatabase(){
+        databaseReference.child("Pessoa").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listPessoa.clear();
+                for(DataSnapshot objSnapshot:dataSnapshot.getChildren()){
+                    Pessoa p = objSnapshot.getValue(Pessoa.class);
+                    listPessoa.add(p);
+                }
+                arrayAdapterPessoa = new ArrayAdapter<Pessoa>(MainActivity.this, android.R.layout.simple_list_item_1, listPessoa);
+                listV_dados.setAdapter(arrayAdapterPessoa);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
     private void inicializarFirebase(){
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
     }
 
@@ -57,6 +102,22 @@ public class MainActivity extends AppCompatActivity {
             p.setEmail(edtEmail.getText().toString());
 
             databaseReference.child("Pessoa").child(p.getUid()).setValue(p);
+            limparCampos();
+        }
+
+        else if(id == R.id.menu_atualiza){
+            Pessoa p = new Pessoa();
+            p.setUid(pessoaSelecionada.getUid());
+            p.setNome(edtNome.getText().toString().trim());
+            p.setEmail(edtEmail.getText().toString().trim());
+            databaseReference.child("Pessoa").child(p.getUid()).setValue(p);
+        }
+
+        else if(id == R.id.menu_deleta){
+            Pessoa p = new Pessoa();
+            p.setUid(pessoaSelecionada.getUid());
+            databaseReference.child("Pessoa").child(p.getUid()).removeValue();
+
             limparCampos();
         }
         return true;
