d79ee52770352e0892515d021116e8acd1e14d3d
Elimar Abreu <elimardeabreu4@hotmail.com>
2019-08-14 23:30:33 -0400

    Activities created

---
+++ app/src/main/java/com/example/cm_sgdr/MainActivity_cadastro1.java
@@ -0,0 +1,14 @@
+package com.example.cm_sgdr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_cadastro1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_cadastro1);
+    }
+}
