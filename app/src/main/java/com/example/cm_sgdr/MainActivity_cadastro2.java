61bf1ef50691f518c4554b0dd38e738ce7d04b49
Elimar Abreu <elimardeabreu4@hotmail.com>
2019-08-18 14:17:51 -0400

    Update Activities

--- app/src/main/java/com/example/cm_sgdr/MainActivity_cadastro2.java
+++ app/src/main/java/com/example/cm_sgdr/MainActivity_cadastro2.java
@@ -2,7 +2,10 @@ package com.example.cm_sgdr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity_cadastro2 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity_cadastro2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_cadastro2);
+
+        Button confirmar = (Button) findViewById(R.id.button_cadastro2_confirmar);
+
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity_cadastro2.this, MainActivity_principal.class);
+                startActivity(it);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cm_sgdr/MainActivity_login.java
+++ app/src/main/java/com/example/cm_sgdr/MainActivity_login.java
@@ -2,7 +2,10 @@ package com.example.cm_sgdr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity_login extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        Button cadastrar = (Button) findViewById(R.id.button_login_cadastrar);
+        Button entrar = (Button) findViewById(R.id.button_login_entrar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity_login.this, MainActivity_cadastro1.class);
+                startActivity(it);
+            }
+        });
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity_login.this, MainActivity_principal.class);
+                startActivity(it);
+            }
+        });
+
     }
+
 }
+
