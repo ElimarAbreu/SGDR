2652ecbdb577bcdb107f42da0a62169cdbb1674e
Elimar Abreu <elimardeabreu4@hotmail.com>
2019-08-19 20:21:40 -0400

    Update Activities

--- app/src/main/java/com/example/cm_sgdr/MainActivity_cadastro1.java
+++ app/src/main/java/com/example/cm_sgdr/MainActivity_cadastro1.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity_cadastro1 extends AppCompatActivity {
 
@@ -22,5 +24,12 @@ public class MainActivity_cadastro1 extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+        Spinner spinner_republica = (Spinner)
+        findViewById(R.id.spinner_cadastro1_republica);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.menu, android.R.layout.simple_spinner_item);
+        spinner_republica.setAdapter(adapter);
+
     }
 }
